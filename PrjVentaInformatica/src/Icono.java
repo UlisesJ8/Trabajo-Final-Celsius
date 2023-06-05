2816efd28f1140a4fe6217359c89ba00269e0890
Ulises Santana <ulisesjs8@gmail.com>
2023-06-05 02:39:05 -0300

    Cargando proyecto
    Primer commit del trabajo creado para certificacion de celsius y la utn

---
+++ PrjVentaInformatica/src/Icono.java
@@ -0,0 +1,33 @@
+
+import java.awt.Frame;
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JPanel;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author Equipo
+ */
+public class Icono {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Frame f = new Frame();
+//       JButton button = new JButton("New Window")
+       f.setVisible(true);
+
+       
+       
+       
+       
+    }
+
+}
